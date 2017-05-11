8fb836cf7dada501d129adba6e89b2d4c6a32173
hihi5404 <hihi5404@gmail.com>
2017-05-11 04:37:48 -0400

    finish list book feature

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,11 @@ class BooksController < ApplicationController
   def index 
     @books=Book.all
   end
-    
+  
+  def show
+   @book=Book.find_by(id: params[:id])
+  end
+  
    def new
     @book=Book.new
   end
